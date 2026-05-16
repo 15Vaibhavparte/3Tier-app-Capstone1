1d2afd632049f740b3ab0036e669bdad062fb88c
15Vaibhavparte <vaibhavparte2@gmail.com>
2026-05-16 12:38:51 +0530

    added 1st commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'three-tierdb.cluster-c5ago44wu8uq.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Vaibhav2026',
+    DB_DATABASE : 'webappdb'
+});
