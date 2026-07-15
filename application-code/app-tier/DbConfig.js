f7a6cca1b81f12768b3b653e34a6d41798594ca2
15Vaibhavparte <vaibhavparte2@gmail.com>
2026-07-15 19:11:32 +0530

    refactor: update database configuration to use environment variables and add deployment script

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,11 @@
 module.exports = Object.freeze({
-    DB_HOST : 'three-tierdb.cluster-c5ago44wu8uq.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'Vaibhav2026',
-    DB_DATABASE : 'webappdb'
+    // If a Docker environment variable exists, use it; otherwise, fall back to your default string.
+    DB_HOST: process.env.DB_HOST ,
+    DB_USER: process.env.DB_USER ,
+    
+    // Notice we mapped this to DB_PASSWORD to match standard naming conventions, 
+    // but it falls back gracefully to your secure password string.
+    DB_PWD: process.env.DB_PASSWORD ,
+    
+    DB_DATABASE: process.env.DB_NAME
 });
